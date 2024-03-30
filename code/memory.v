0d3cc65c247fded6f3ca7b66308d91debcb302e4
Adam Taback <122375166+adamrt27@users.noreply.github.com>
2024-03-30 17:55:00 -0400

    LAB 1 works baby

--- code/memory.v
+++ code/memory.v
@@ -19,7 +19,35 @@ module memory(CLK, reset, MemRead, MemWrite, ADDR, Data_in, Data_out);
     reg[15:0] mem[1023:0];
     always @(posedge CLK) begin
         if (reset) begin        // on reset, set all registers to 0
-            mem[0] = 16'b0010011111100111; // set first thing to be R1 = R1 + 1111 (binary)
+
+            // the following code adds numbers from 1 to 30 and places result in r4
+            mem[0] = 16'b0010010000100111; 
+            mem[1] = 16'b0110111110100111;
+            mem[2] = 16'b0100100100000001;
+            mem[3] = 16'b1001001000000001;
+            mem[4] = 16'b0100010100000000;
+            mem[5] = 16'b1001000100000000;
+            mem[6] = 16'b1010100110001110;
+            mem[7] = 16'b0000000010010100;
+            mem[8] = 16'b0001000100010001; // add in extra 1 at 12th bit to show r4 (does effect BR)
+
+            /* equivalent code:
+
+            _start:
+                movi r1, 1 /* put 1 in r1 
+                movi r3, 29
+                movi r2, 0
+                movi r12, 0
+                
+            LOOP:
+                add r2, r1, r2 /* store current value of loop in r2 
+                add r12, r12, r2 /* add r2 to r12 
+                ble r2, r3, LOOP /* loops back to start of loop if current counter value less than 30 
+                
+            done: br done
+            */
+
+            /*mem[0] = 16'b0010011111100111; // set first thing to be R1 = R1 + 1111 (binary)
             mem[1] = 16'b0010011111100111; // R1 = 1111 + 1111 = 011110
             mem[2] = 16'b0010010001101100; // R1 = 11110 << 11 = 11110000
             mem[3] = 16'b0100100010000001; // R2 = R2 - R1 = 0 - 11110000
@@ -37,7 +65,7 @@ module memory(CLK, reset, MemRead, MemWrite, ADDR, Data_in, Data_out);
             // add iloop: BR 7
             mem[31] = 16'b00000011111110001; 
             mem[12] = 0;
-            mem[13] = 0;
+            mem[13] = 0; */
 
             /*
             Nios II equiv:
